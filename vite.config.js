949cfc76cc7763174dc747f7a28539940c2d2c0f
Your Name <you@example.com>
2025-07-25 01:41:18 -0400

    no se que cambio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/RentSpot",
-  plugins: [react()],
+  plugins: [react()], //comentario
 });
