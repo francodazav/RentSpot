9702858634f8f952e85cd48752d2d5a14ffbb8f1
Your Name <you@example.com>
2025-07-26 00:55:02 -0400

    agregue build outdir : dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  outDir: "dist",
   plugins: [react()], //comentario
+  build: {
+    outDir: "dist",
+  },
 });
